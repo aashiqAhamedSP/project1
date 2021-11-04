cb943ad8f850fd25e52682e924efcf62798064e9
aashiqAhamedSP <67874080+aashiqAhamedSP@users.noreply.github.com>
2021-11-05 01:32:26 +0530

    Update hostfileconfig.sh

--- hostfileconfig.sh
+++ hostfileconfig.sh
@@ -5,4 +5,3 @@ echo "$localip ansible_connection=local" >> /etc/ansible/hosts
 docip=$(az vm show -d -g jenkin-rg -n docvm --query privateIps -o tsv)
 echo "[remotehost]" >> /etc/ansible/hosts
 echo "$docip ansible_connection=ssh" >> /etc/ansible/hosts
-ssh vmadmin@$docip -i /home/vmadmin/.ssh/authorized_keys/id_rsa
