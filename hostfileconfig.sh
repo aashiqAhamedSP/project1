2ef632a6eba2106d9a4c8fd4de3f5b048d514f76
aashiqAhamedSP <67874080+aashiqAhamedSP@users.noreply.github.com>
2021-11-05 03:29:02 +0530

    Update hostfileconfig.sh

--- hostfileconfig.sh
+++ hostfileconfig.sh
@@ -5,4 +5,5 @@ echo "$loclip ansible_connection=local" >> /etc/ansible/hosts
 docip=$(az vm show -d -g jenkin-rg -n docvm --query privateIps -o tsv)
 echo "[remotehost]" >> /etc/ansible/hosts
 echo "$docip ansible_connection=ssh" >> /etc/ansible/hosts
+rm /home/vmadmin/.ssh/known_hosts
 ssh -o "StrictHostKeyChecking no" vmadmin@$docip -i /home/vmadmin/.ssh/authorized_keys/id_rsa
